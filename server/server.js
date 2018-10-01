18056c36cc690321a110af042e3691ef894c94f3
RyanADesigns <[ryan.stupey@gmail.com]>
2018-10-01 16:04:02 -0700

    added express static

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ app.use(bodyParser.json())
 
 app.use(cors());
 
+app.use( express.static( `${__dirname}/../build` ) );
+
 app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '../build/index.html'));
 });
